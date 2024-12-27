3256859e3c5e63b7085db0953e45365c93377a77
amie <ziweigeger@163.com>
2024-12-27 19:36:33 +0800

    添加项目文件。

---
+++ Form1.Designer.cs
@@ -0,0 +1,194 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+
+namespace Mines
+{
+	partial class Form1
+	{
+		/// <summary>
+		///  Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		///  Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		///  Required method for Designer support - do not modify
+		///  the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.components = new System.ComponentModel.Container();
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+			this.button_result = new System.Windows.Forms.Button();
+			this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+			this.label_timer = new System.Windows.Forms.Label();
+			this.radio_level1 = new System.Windows.Forms.RadioButton();
+			this.radio_level2 = new System.Windows.Forms.RadioButton();
+			this.radio_level3 = new System.Windows.Forms.RadioButton();
+			this.Board = new System.Windows.Forms.Panel();
+			this.panel1 = new System.Windows.Forms.Panel();
+			this.button_restart = new System.Windows.Forms.Button();
+			this.panel1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// button_result
+			// 
+			this.button_result.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+			this.button_result.Dock = System.Windows.Forms.DockStyle.Top;
+			this.button_result.Enabled = false;
+			this.button_result.FlatAppearance.BorderColor = System.Drawing.Color.White;
+			this.button_result.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.button_result.ImageIndex = 2;
+			this.button_result.ImageList = this.imageList1;
+			this.button_result.Location = new System.Drawing.Point(0, 0);
+			this.button_result.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+			this.button_result.Name = "button_result";
+			this.button_result.Size = new System.Drawing.Size(162, 94);
+			this.button_result.TabIndex = 2;
+			this.button_result.UseVisualStyleBackColor = false;
+			this.button_result.UseWaitCursor = true;
+			// 
+			// imageList1
+			// 
+			this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+			this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+			this.imageList1.Images.SetKeyName(0, "lose.jpg");
+			this.imageList1.Images.SetKeyName(1, "win.jpg");
+			this.imageList1.Images.SetKeyName(2, "default.jpg");
+			// 
+			// label_timer
+			// 
+			this.label_timer.AutoSize = true;
+			this.label_timer.Dock = System.Windows.Forms.DockStyle.Top;
+			this.label_timer.Font = new System.Drawing.Font("微软雅黑", 13.8F, System.Drawing.FontStyle.Bold);
+			this.label_timer.Location = new System.Drawing.Point(0, 136);
+			this.label_timer.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+			this.label_timer.Name = "label_timer";
+			this.label_timer.Size = new System.Drawing.Size(146, 31);
+			this.label_timer.TabIndex = 4;
+			this.label_timer.Text = "计时 00：00";
+			// 
+			// radio_level1
+			// 
+			this.radio_level1.AutoSize = true;
+			this.radio_level1.Checked = true;
+			this.radio_level1.Dock = System.Windows.Forms.DockStyle.Top;
+			this.radio_level1.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F);
+			this.radio_level1.Location = new System.Drawing.Point(0, 229);
+			this.radio_level1.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+			this.radio_level1.Name = "radio_level1";
+			this.radio_level1.Size = new System.Drawing.Size(162, 31);
+			this.radio_level1.TabIndex = 5;
+			this.radio_level1.TabStop = true;
+			this.radio_level1.Text = "初级（10阶）";
+			this.radio_level1.UseVisualStyleBackColor = true;
+			this.radio_level1.Click += new System.EventHandler(this.radio_level1_CheckedChanged);
+			// 
+			// radio_level2
+			// 
+			this.radio_level2.AutoSize = true;
+			this.radio_level2.Dock = System.Windows.Forms.DockStyle.Top;
+			this.radio_level2.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F);
+			this.radio_level2.Location = new System.Drawing.Point(0, 198);
+			this.radio_level2.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+			this.radio_level2.Name = "radio_level2";
+			this.radio_level2.Size = new System.Drawing.Size(162, 31);
+			this.radio_level2.TabIndex = 6;
+			this.radio_level2.Text = "中级（15阶）";
+			this.radio_level2.UseVisualStyleBackColor = true;
+			this.radio_level2.Click += new System.EventHandler(this.radio_level2_CheckedChanged);
+			// 
+			// radio_level3
+			// 
+			this.radio_level3.AutoSize = true;
+			this.radio_level3.Dock = System.Windows.Forms.DockStyle.Top;
+			this.radio_level3.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F);
+			this.radio_level3.Location = new System.Drawing.Point(0, 167);
+			this.radio_level3.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+			this.radio_level3.Name = "radio_level3";
+			this.radio_level3.Size = new System.Drawing.Size(162, 31);
+			this.radio_level3.TabIndex = 7;
+			this.radio_level3.Text = "高级（20阶）";
+			this.radio_level3.UseVisualStyleBackColor = true;
+			this.radio_level3.Click += new System.EventHandler(this.radio_level3_CheckedChanged);
+			// 
+			// Board
+			// 
+			this.Board.AutoSize = true;
+			this.Board.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.Board.Location = new System.Drawing.Point(0, 0);
+			this.Board.Margin = new System.Windows.Forms.Padding(2);
+			this.Board.Name = "Board";
+			this.Board.Size = new System.Drawing.Size(924, 395);
+			this.Board.TabIndex = 8;
+			// 
+			// panel1
+			// 
+			this.panel1.Controls.Add(this.radio_level1);
+			this.panel1.Controls.Add(this.radio_level2);
+			this.panel1.Controls.Add(this.radio_level3);
+			this.panel1.Controls.Add(this.label_timer);
+			this.panel1.Controls.Add(this.button_restart);
+			this.panel1.Controls.Add(this.button_result);
+			this.panel1.Dock = System.Windows.Forms.DockStyle.Right;
+			this.panel1.Location = new System.Drawing.Point(924, 0);
+			this.panel1.Name = "panel1";
+			this.panel1.Size = new System.Drawing.Size(162, 395);
+			this.panel1.TabIndex = 9;
+			// 
+			// button_restart
+			// 
+			this.button_restart.Dock = System.Windows.Forms.DockStyle.Top;
+			this.button_restart.Location = new System.Drawing.Point(0, 94);
+			this.button_restart.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+			this.button_restart.Name = "button_restart";
+			this.button_restart.Size = new System.Drawing.Size(162, 42);
+			this.button_restart.TabIndex = 3;
+			this.button_restart.Text = "重新开始";
+			this.button_restart.UseVisualStyleBackColor = true;
+			this.button_restart.Click += new System.EventHandler(this.button_restart_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(1086, 395);
+			this.Controls.Add(this.Board);
+			this.Controls.Add(this.panel1);
+			this.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+			this.Name = "Form1";
+			this.Text = "扫雷";
+			this.panel1.ResumeLayout(false);
+			this.panel1.PerformLayout();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+		private Button button_result;
+		private Label label_timer;
+		private RadioButton radio_level1;
+		private RadioButton radio_level2;
+		private RadioButton radio_level3;
+		private ImageList imageList1;
+		private Panel Canvas;
+		private Panel Board;
+		private Panel panel1;
+		private Button button_restart;
+	}
+}
---
+++ Form1.cs
@@ -0,0 +1,336 @@
+﻿using System.Collections.Generic;
+using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace Mines
+{
+	public partial class Form1 : Form
+	{
+		private RadioButton[] levels;
+		private List<Button> Field = new List<Button>();
+		private enum BlockState
+		{
+			Init,
+			Flag,
+			Mined
+		};
+		private List<BlockState> FieldState = new List<BlockState>();
+		private List<int> BombMap = new List<int>();
+		private Dictionary<RadioButton, int> gridSize;
+		private DataGridViewTextBoxColumn[] cols;
+		private bool GameOn = false;
+		private bool FirstClick = true;
+		private int countMine = 0;
+		private Stopwatch stopwatch = new Stopwatch();
+		Task taskTimer;
+		CancellationToken tokenTimer;
+		CancellationTokenSource tsTimer;
+		int size = 10;
+		public Form1()
+		{
+			System.Windows.Forms.Control.CheckForIllegalCrossThreadCalls = false;
+
+			InitializeComponent();
+			InitializeSizeDic();
+			InitializeLevels();
+			this.Board.Size = new Size(0, 0);
+			this.Size = new Size(this.panel1.Width + 30, this.panel1.Height);
+			button_result.Paint +=button1_Paint;
+		}
+
+		private void button1_Paint(object sender, PaintEventArgs e)
+		{
+			Button btn = sender as Button;
+			Image img = btn.ImageList.Images[btn.ImageIndex]; // 获取图片
+			
+			// 调整图片大小以适应按钮
+			Image resizedImg = new Bitmap(img, new Size(btn.Width, btn.Height));
+
+			// 绘制图片
+			e.Graphics.DrawImage(resizedImg, 0, 0, btn.Width, btn.Height);
+		}
+		private void InitializeSizeDic()
+		{
+			gridSize = new Dictionary<RadioButton, int>() { [radio_level1] = 10, [radio_level2] = 15, [radio_level3] = 20 };
+		}
+		private void InitializeLevels()
+		{
+			levels = new RadioButton[] { radio_level1, radio_level2, radio_level3 };
+		}
+		private void PaintBlankCanvas()
+		{
+			int a = 50;
+			//获取扫雷棋盘的尺寸，也是埋雷个数
+			foreach (RadioButton rb in levels)
+			{
+				if (rb.Checked)
+				{
+					size = gridSize[rb];
+				}
+			}
+			this.panel1.Dock = DockStyle.Right;
+			Board.Size = new Size(a * (size) + 50, a * (size) + 10);
+			Board.Controls.Clear();
+			Field.Clear();
+			FieldState.Clear();
+
+			for (int i = 0; i < size; i++)
+			{
+				for (int j = 0; j < size; j++)
+				{
+					Button button = new Button()
+					{
+						Location = new Point(i * a, j * a),
+						Size = new Size(a, a),
+						BackColor = Color.White,
+
+					};
+					button.MouseDown += BlockClick;
+
+					Board.Controls.Add(button);
+					Field.Add(button);
+					FieldState.Add(BlockState.Init);
+				}
+			}
+			int boardsize = a*size;
+			int panel1width = panel1.Width;
+			this.Size = new Size(panel1width + boardsize, boardsize);
+		}
+		//po表示第一次单机画布时候的位置
+		private void InitializeCanvas(Point p0)
+		{
+			int size = 0;
+			//获取扫雷棋盘的尺寸，也是埋雷个数
+			foreach (RadioButton rb in levels)
+			{
+				if (rb.Checked)
+				{
+					size = gridSize[rb];
+				}
+			}
+			//Canvas保存棋盘布局
+			//bombs保存炸弹位置
+			List<Point> bombs = new List<Point>(size);
+			for (int i = 0; i < size * size; i++)
+			{
+				BombMap.Add(0);
+			}
+			int x;
+			int y;
+			//随机选择size个不同的点放置炸弹
+			for (int i = 0; i < size; i++)
+			{
+				Random random = new Random();
+				x = random.Next(size);
+				y = random.Next(size);
+				//注意炸弹不能放在初始点下的位置
+				while (bombs.Contains(p0))
+				{
+					x = random.Next(size);
+					y = random.Next(size);
+				}
+				while (bombs.Contains(new Point(x, y)))
+				{
+					x = random.Next(size);
+					y = random.Next(size);
+				}
+				bombs.Add(new Point(x, y));
+			}
+
+			//数一数空格旁边有几个炸弹
+			for (int i = 0; i < size; i++)
+			{
+				for (int j = 0; j < size; j++)
+				{
+					if (bombs.Contains(new Point(i, j)))
+					{
+						BombMap[i * size + j] = int.MaxValue;
+						continue;
+					}
+
+					//初始化为0
+					int count = 0;
+					for (int dx = -1; dx <= 1; dx++)
+					{
+						//count存放炸弹数量
+
+						for (int dy = -1; dy <= 1; dy++)
+						{
+							if (dx == 0 && dy == 0) continue;
+							int nx = i + dx;
+							int ny = j + dy;
+							if (nx < 0 || nx >= size || ny < 0 || ny >= size) continue;
+							if (bombs.Contains(new Point(nx, ny)))
+							{
+								count++;
+							}
+						}
+
+					}
+					BombMap[i * size + j] = count;
+				}
+			}
+		}
+		#region radioButton互斥属性实现
+		private void radio_level1_CheckedChanged(object sender, EventArgs e)
+		{
+			radio_level1.Checked = true;
+			radio_level2.Checked = false;
+			radio_level3.Checked = false;
+		}
+
+		private void radio_level2_CheckedChanged(object sender, EventArgs e)
+		{
+			radio_level2.Checked = true;
+			radio_level1.Checked = false;
+			radio_level3.Checked = false;
+		}
+
+		private void radio_level3_CheckedChanged(object sender, EventArgs e)
+		{
+			radio_level3.Checked = true;
+			radio_level2.Checked = false;
+			radio_level1.Checked = false;
+		}
+		#endregion
+		private string TimerLabel(int second)
+		{
+			//若时长超过1小时
+			if (second >= 60 * 60)
+			{
+				return "超过1小时";
+			}
+			string min = Convert.ToString(second / 60).PadLeft(2, '0');
+			string sec = Convert.ToString(second % 60).PadLeft(2, '0');
+			return min + ":" + sec;
+		}
+		public async Task RunAsyncTimer(CancellationToken ct)
+		{
+			if (ct.IsCancellationRequested) return;
+			await Task.Run(() => UpdateTImer(ct), ct);
+		}
+		private void UpdateTImer(CancellationToken token)
+		{
+			while (!token.IsCancellationRequested)
+			{
+				label_timer.Invoke((EventHandler)delegate
+				{
+					label_timer.Text = "计时  " + TimerLabel((int)(stopwatch.ElapsedMilliseconds / 1000));
+				});
+				Thread.Sleep(200);
+			}
+
+		}
+		private void button_restart_Click(object sender, EventArgs e)
+		{
+			Thread.Sleep(500);
+			if (!GameOn)
+			{
+				GameOn = true;
+				FirstClick = true;
+				button_result.ImageIndex = 2;
+				stopwatch.Reset();
+				stopwatch.Start();
+				tsTimer = new CancellationTokenSource();
+				tokenTimer = tsTimer.Token;
+				taskTimer = RunAsyncTimer(tokenTimer);
+				PaintBlankCanvas();
+				button_restart.Text = "放弃";
+
+			}
+			else
+			{
+				GameOn = false;
+				stopwatch.Stop();
+				tsTimer.Cancel();
+				button_result.ImageIndex = 0;
+				button_restart.Text = "重新开始";
+			}
+		}    
+		private void BlockClick(object sender, MouseEventArgs e)
+		{
+			//若未开始游戏，点击无效
+			if (!GameOn) return;
+			Button b = sender as Button;
+			//若点击的不是button控件，无效
+			if (b == null) return;
+			int id = Field.IndexOf(b);
+			//若点击的button不属于Field列表，无效
+			if (id == -1) return;
+			//若当前块被置标志，不动作
+			if (e.Button == MouseButtons.Right)
+			{
+				//若当前块已经点开，返回
+				if (FieldState[id] == BlockState.Mined) return;
+				if (FieldState[id] != BlockState.Flag)
+				{
+					FieldState[id] = BlockState.Flag;
+					b.Text = "F";
+					b.BackColor = Color.Yellow;
+				}
+				else
+				{
+					FieldState[id] = BlockState.Init;
+					b.Text = "";
+					b.BackColor = Color.White;
+				}
+			}
+			else
+			{
+				if (FieldState[id] == BlockState.Flag) return;
+				FieldState[id] = BlockState.Mined;
+				int row = id / size;
+				int col = id % size;
+				if (FirstClick)
+				{
+					InitializeCanvas(new Point(row, col));
+					FirstClick = false;
+					countMine = 1;
+				}
+
+				int curr = BombMap[row * size + col];
+				if (curr == int.MaxValue)
+				{
+					b.Text = "*";
+					b.BackColor = Color.Red;
+					GameOn = false;
+					button_restart.Text = "重新开始";
+					tsTimer?.Cancel();
+					button_result.ImageIndex = 0;
+					MessageBox.Show("You died", "提示", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+					return;
+
+				}
+				else
+				{
+					if (curr == 0)
+					{
+						b.Text = "";
+					}
+					else
+					{
+						b.Text = curr.ToString();
+					}
+					b.BackColor = Color.Green;
+					countMine++;
+				}
+
+				if (countMine == size * size - size)
+				{
+					button_result.ImageIndex = 1;
+					MessageBox.Show("You WON");
+					GameOn = false;
+					return;
+				}
+			}
+
+		}
+
+	}
+}
